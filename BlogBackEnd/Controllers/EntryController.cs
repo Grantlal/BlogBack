fc8e203ac862a7f3c7a2511effc1ae68c380310a
Grant <granttlalonde@gmail.com>
2020-08-13 12:56:21 -0700

    Starting the ball Rolling

---
+++ BlogBackEnd/Controllers/EntryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlogBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EntryController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Connected to Entry";
+        }
+    }
+}
