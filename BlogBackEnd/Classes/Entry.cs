35344b2945c02de2b1363087124d5f619aef0423
Grant <granttlalonde@gmail.com>
2021-04-24 13:23:32 -0700

    Added Entry Class

---
+++ BlogBackEnd/Classes/Entry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogBackEnd.Classes
+{
+    public class Entry
+    {
+        private string Title;
+        private string Body;
+
+        public Entry() { }
+
+        public Entry(string title, string body)
+        {
+            Title = title;
+            Body = body;
+        }
+
+        public string getTitle()
+        {
+            return Title;
+        }
+
+        public void setTitle(string title)
+        {
+            Title = title;
+        }
+        public string getBody()
+        {
+            return Body;
+        }
+
+        public void setBody(string body)
+        {
+            Body = body;
+        }
+
+
+
+    }
+}
